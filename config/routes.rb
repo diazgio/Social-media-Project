95d0e80075481ddd7d41f8549e6de87256bd7065
Giordano Dìaz <diazgiordano@gmail.com>
2020-10-30 17:39:32 -0500

    Create Friendship model with foreign key

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :create] do
-    resources :comments, only: [:create]
-    resources :likes, only: [:create, :destroy]
+  resources :users, only: %i[index show]
+  resources :posts, only: %i[index create] do
+    resources :comments, only: %i[create]
+    resources :likes, only: %i[create destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
